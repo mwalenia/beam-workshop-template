04f6a3f93f7f6e6ecb69a8b8361f9daf7de7c451
Michal Walenia <michal.walenia@polidea.com>
2020-12-02 17:35:09 +0100

    Add reading data from BigQuery to the pipeline

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
@@ -6,6 +7,8 @@ public class Main {
   public static void main(String[] args) {
     PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
     Pipeline p = Pipeline.create(options);
+    p.apply("Read from BigQuery", BigQueryIO.readTableRows()
+        .fromQuery("SELECT * FROM [bigquery-public-data.chicago_taxi_trips.taxi_trips] WHERE trip_seconds > 0 limit 5000"));
     p.run().waitUntilFinish();
   }
 }
